ce6455cc22dd72df69bae072c879af9a0d6d2cb5
Douglas <douglasfmr1@gmail.com>
2022-05-30 19:14:39 -0300

    feat(re6): requisito 6 completo

--- src/Battle/Battle.ts
+++ src/Battle/Battle.ts
@@ -1,13 +1,11 @@
-// import Fighter from '../Fighter';
-// teste //
+import Fighter from '../Fighter/Fighter';
 
-// abstract class Battle {
-//   constructor(protected player: Fighter) { }
+abstract class Battle {
+  constructor(protected player: Fighter) {}
 
-//   // Should return 1 if player wins, -1 otherwise
-//   fight(): number {
-//     return this.player.lifePoints === -1 ? -1 : 1;
-//   }
-// }
+  fight(): number {
+    return this.player.lifePoints === -1 ? -1 : 1;
+  }
+}
 
-// export default Battle;
+export default Battle;
